0d69e1204a158d1cbb0203c3145e9c8ebfd03b17
Nathan Woltman <nwoltman@outlook.com>
2016-12-07 14:00:51 -0500

    tasks: Update fixdocs task to improve Examples docs
    Now it adds a newline before the first example so the example
    header isn't squeezed against whatever comes before it.

--- tasks/fixdocs.js
+++ tasks/fixdocs.js
@@ -5,7 +5,9 @@ const os = require('os');
 
 module.exports = function(grunt) {
   grunt.registerTask('fixdocs', 'Fixes any problems with the generated documentation', () => {
-    const docs = fs.readFileSync('README.md', 'utf8').replace(/\r\n|\r|\n/g, os.EOL);
+    const docs = fs.readFileSync('README.md', 'utf8')
+      .replace(/\r\n|\r|\n/g, os.EOL)
+      .replace(new RegExp('  ' + os.EOL + '(?=\\*\\*Example\\*\\*)', 'g'), os.EOL + os.EOL);
     fs.writeFileSync('README.md', docs);
     grunt.log.ok('Fixed docs');
   });
