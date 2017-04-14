fa8960b128340a205cd28fd15e49b190f092aca7
Nathan Woltman <nwoltman@outlook.com>
2017-04-14 14:09:14 -0400

    lib: Fix bug when migrating a table with no foreign keys to having some

---
+++ test/integration/addForeignKeys.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const MySQLPlus = require('../../lib/MySQLPlus');
+
+const config = require('../config');
+
+const ColTypes = MySQLPlus.ColTypes;
+
+describe('when migrating a table with no foreign keys to having some foreign keys', function() {
+
+  const pool = MySQLPlus.createPool(config);
+  const pool2 = MySQLPlus.createPool(config);
+
+  pool.defineTable('add_foreign_keys_foreign', {
+    columns: {
+      id: ColTypes.int().notNull().primaryKey(),
+    },
+  });
+  pool.defineTable('add_foreign_keys_main', {
+    columns: {
+      id: ColTypes.int().notNull().index(),
+    },
+  });
+  pool2.defineTable('add_foreign_keys_main', {
+    columns: {
+      id: ColTypes.int().notNull().index(),
+    },
+    foreignKeys: {
+      id: 'add_foreign_keys_foreign.id',
+    },
+  });
+
+  before(done => {
+    pool.sync(err => {
+      if (err) {
+        throw err;
+      }
+
+      pool.end(done);
+    });
+  });
+
+  after(done => {
+    pool2.end(done);
+  });
+
+  it('should not error', done => {
+    pool2.sync(done);
+  });
+
+});
